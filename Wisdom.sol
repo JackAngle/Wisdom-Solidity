1e3ee991a87bfa101c38583b6c75de1e6d177536
JackAngle <43754974+JackAngle@users.noreply.github.com>
2022-02-12 14:43:57 +0700

    Add a delegatecall smart contract

--- Wisdom.sol
+++ Wisdom.sol
@@ -13,18 +13,23 @@ contract Wisdom {
     string  private answer = "";
     mapping (address => uint) public sentAmount;
 
-    constructor(string memory _answer) {
+    // constructor(string memory _answer) {
+    //     owner = payable(msg.sender);
+    //     answer = _answer;
+    //     /**
+    //     * BELOW is the original code, which i was stupidly hashing
+    //     * then reallized user can't decrypt the hash for answer
+    //     */
+    //     // bytes32  theAnswer;
+    //     // assembly{
+    //     //     theAnswer := keccak256(add(_answer, 0x20), mload(_answer))
+    //     // }
+    //     // answer = theAnswer;
+    // }
+
+    constructor() {
         owner = payable(msg.sender);
-        answer = _answer;
-        /**
-        * BELOW is the original code, which i was stupidly hashing
-        * then reallized user can't decrypt the hash for answer
-        */
-        // bytes32  theAnswer;
-        // assembly{
-        //     theAnswer := keccak256(add(_answer, 0x20), mload(_answer))
-        // }
-        // answer = theAnswer;
+        answer = "ZERO";
     }
 
 
@@ -65,7 +70,7 @@ contract Wisdom {
     event WithdrawAssembly(uint timestamp);
     function withdrawAssembly() external {
         require(msg.sender == owner, "Only owner can withdraw from this smart contract");
-        address payable destAddr = owner; // I wish we can access state variable directly :'(
+        address payable destAddr = owner;
         bool result;
         assembly {
             result := call(
@@ -132,4 +137,23 @@ contract Wisdom {
     }
 
 
+}
+
+// Change context of question-answer
+// If _answer == yes => emit Right
+// Else if _answer == Wisdom's answer a.k.a  => emit Wrong
+// Else emit Wrong
+// P/s: This contract will use Wisdom's emit messages at the moment
+contract DelegateCallWisdom {
+    address payable public owner;
+    string public question = "Do you know delegatecall?";
+    string private answer = "yes";
+    mapping (address => uint) public sentAmount;
+
+    function tryToAnswerDelegateCall(address _wisdomAddress, string calldata _answer) external payable {
+        (bool success, ) = _wisdomAddress.delegatecall(
+            abi.encodeWithSelector(Wisdom.tryToAnswer.selector, _answer)
+        );
+        require(success, "Delegate Call not success");
+    }
 }
